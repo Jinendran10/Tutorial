78ef352cfdec16aed1c26bcd9b8eca685c1e30b5
Jinendran10 <jinendranjinu10@gmail.com>
2024-08-12 11:40:11 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class prime
+{
+	public static void main(String args[])
+	{
+		Scanner s1=new Scanner(System.in);
+		int n,i,j,s=0;
+		int p[]=new int[50];
+		System.out.println("Enter the limit:");
+		n=s1.nextInt();
+		System.out.println("The prime numbers upto "+n+"are:");
+		for(i=1;i<n;i++)
+		{
+			for(j=1;j<=i/2;j++)
+			{
+				if(i%j==0)
+				{
+					s=1;
+					break;
+				}
+				if(s==0)
+				{
+					p[i]=i;
+				}
+			}
+		}
+		for(i=1;i<n;i++)
+		{
+			if(p[i]!=0)				
+			{
+				System.out.print(p[i]+"\t");
+			}
+		}
+		s1.close();
+	}		
+}	
